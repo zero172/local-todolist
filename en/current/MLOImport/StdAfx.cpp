d67927e12bf816e807292499629b2285aaf432ca
jamesfancy <jamesfancy@bb06807d-9325-0410-9e1a-69f524c7d53b>
2007-08-28 10:40:45 +0000

    leave resource files and projects only.

--- en/current/MLOImport/StdAfx.cpp
+++
@@ -1,8 +0,0 @@
-// stdafx.cpp : source file that includes just the standard includes
-//	MLOImport.pch will be the pre-compiled header
-//	stdafx.obj will contain the pre-compiled type information
-
-#include "stdafx.h"
-
-
-
